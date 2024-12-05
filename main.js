8be488d22e70a6a557a83c8d2b168213b90bfd05
peachyem <125699041+peachyem@users.noreply.github.com>
2024-12-05 16:36:25 -0600

    Update main.js

--- main.js
+++ main.js
@@ -15,9 +15,8 @@ function processData(strData){
     return dictionary;
 }
 
-
+const form = document.getElementById("SpellChecking");
 form.addEventListener("submit", (event) =>{
-    const form = document.getElementById("SpellChecking");
     const textBox = document.getElementById("word");
     dict = fetch("dictionary.txt").then(convertData).then(processData);
     event.preventDefault();
